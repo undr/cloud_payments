e30f378a99e0f3934d88be2210dccd53d384c741
undr <undr@yandex.ru>
2014-12-25 23:36:04 +0700

    Webmock vs CodeClimate. Part 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,7 @@ end
 require 'bundler'
 Bundler.require(:default, :test)
 
-WebMock.disable_net_connect!(allow: 'https://codeclimate.com/test_reports')
+WebMock.disable_net_connect!
 
 Dir["./spec/support/**/*.rb"].each { |f| require f }
 
@@ -22,6 +22,8 @@ end
 RSpec.configure do |config|
   config.mock_with :rspec
   config.include CloudPayments::RSpec::Helpers
+
+  config.after(:suite){ WebMock.allow_net_connect! }
 
   # config.around :each, time_freeze: ->(v){ v.is_a?(Date) || v.is_a?(Time) || v.is_a?(String) } do |example|
   #   datetime = if example.metadata[:time_freeze].is_a?(String)
