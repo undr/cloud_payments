e523f28e60dba6fea28103715a6d33ce651d0ab0
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-03-08 13:36:47 +0300

    Add orders
    See https://cloudpayments.ru/Docs/Api#createOrder

--- lib/cloud_payments/models.rb
+++ lib/cloud_payments/models.rb
@@ -3,6 +3,7 @@ require 'cloud_payments/models/like_subscription'
 require 'cloud_payments/models/secure3d'
 require 'cloud_payments/models/transaction'
 require 'cloud_payments/models/subscription'
+require 'cloud_payments/models/order'
 
 require 'cloud_payments/models/on_recurrent'
 require 'cloud_payments/models/on_pay'
