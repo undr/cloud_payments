3cb85803d93330929fe4db76d8561dd4b1f2c4c9
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-03-14 11:48:09 +0300

    Fix date transformer to accept dates

--- lib/cloud_payments/models/model.rb
+++ lib/cloud_payments/models/model.rb
@@ -5,20 +5,9 @@ module CloudPayments
   class Model < Hashie::Trash
     include Hashie::Extensions::IgnoreUndeclared
 
-    DateTimeTransform  = ->(v) { DateTime.parse(v) if v }
-    DecimalTransform  = ->(v) { v.to_d if v }
-
-    BooleanTransform  =
-      lambda do |v|
-        if v == true || v == false || v.nil?
-          v
-        elsif v == '0'.freeze
-          false
-        elsif v == '1'.freeze
-          true
-        end
-      end
-
-    IntegralTransform = ->(v) { v.to_i if v }
+    DateTimeTransform = ->(v) { DateTime.parse(v) if v&.respond_to? :to_s }
+    DecimalTransform  = ->(v) { v&.to_d }
+    IntegralTransform = ->(v) { v&.to_i }
+    BooleanTransform  = ->(v) { (v == '0') ? false : !!v }
   end
 end
